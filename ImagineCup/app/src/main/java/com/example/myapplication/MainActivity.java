05f9e5f09316d11c0e01888f3c2b866148d77da3
박효완 <qkrgy1206@naver.com>
2017-12-29 02:11:41 +0900

    라즈베리 파이에서 스트리밍 서버 mjpea 설치 후 안드로이드에서 작업 시작

--- ImagineCup/app/src/main/java/com/example/myapplication/MainActivity.java
+++ ImagineCup/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new Button.OnClickListener(){
+
+                                   @Override
+                                   public void onClick(View view) {
+                                       Intent intent = new Intent(getApplicationContext(), StreamingActivity.class);
+                                       startActivity(intent);
+                                   }
+                               }
+        );
     }
 }
---
+++ ImagineCup/app/src/main/java/com/example/myapplication/StreamingActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by woga1 on 2017-12-28.
+ */
+
+public class StreamingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_streaming);
+    }
+
+}
