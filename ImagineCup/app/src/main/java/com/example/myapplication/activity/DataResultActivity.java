6acc55afedecda5cbc3f75567b1c06d3778ba320
Yookmoonsu <yookmoon@naver.com>
2018-01-24 00:21:40 +0900

    DataResultActivity Background Service change and WindowManager View add

--- ImagineCup/app/src/main/java/com/example/myapplication/activity/DataResultActivity.java
+++ ImagineCup/app/src/main/java/com/example/myapplication/activity/DataResultActivity.java
@@ -1,13 +1,25 @@
 package com.example.myapplication.activity;
 
-import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.os.Bundle;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
@@ -16,31 +28,83 @@ import com.example.myapplication.etc.LoadManager;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by woga1 on 2018-01-02.
  */
 
-public class DataResultActivity extends Activity {
-
+public class DataResultActivity extends Service {
     NotifyBabyWarningResult babyWarningResult;
-    public TextView textView;
+    SoundPool soundPool;
+    AudioManager audioManager;
+    int soundId;
+    int streamId;
+    private View mView;
+    private WindowManager mManager;
+    private WindowManager.LayoutParams mParams;
+    boolean play = false;
+    Timer timer;
+    TimerTask timerTask;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+    public void onCreate() {
+        super.onCreate();
 
-        babyWarningResult = new NotifyBabyWarningResult(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // 롤리팝 이전은 단순히 생성자로 생성 롤리팝 이후에는 Builder()로 생성
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            soundPool = new SoundPool.Builder().setAudioAttributes(audioAttributes).setMaxStreams(1).build(); // 동시에 재생할 수 있는 갯수
+        } else {
+            soundPool = new SoundPool(1, AudioManager.STREAM_NOTIFICATION, 0);
+        }
 
-        babyWarningResult.execute("");
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        soundId = soundPool.load(this, R.raw.alarm, 1);
 
-        textView = (TextView) findViewById(R.id.testText);
+        LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mView = mInflater.inflate(R.layout.background_service, null);
 
-        Log.e("LoadManger", "end");
+        // 뷰 터치가능 및 포커스 가지지 않게 함
+        mParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        mManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mManager.addView(mView, mParams);
+
+        Button btnConfirm = (Button) mView.findViewById(R.id.btnConfirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.stop(streamId);
+                mView.setVisibility(View.INVISIBLE);
+                play = false;
+            }
+        });
+
+        mView.setVisibility(View.INVISIBLE);
+
+        final Context context = this;
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                babyWarningResult = new NotifyBabyWarningResult(context);
+                babyWarningResult.execute("");
+            }
+        };
+
+        timer.schedule(timerTask, 1000, 7000);
     }
 
     //웹에서 데이터를 가져오기 전에 먼저 네트워크 상태부터 확인
-
     public void conntectCheck() {
 
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -66,8 +130,13 @@ public class DataResultActivity extends Activity {
         }
     }
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-   private class NotifyBabyWarningResult extends AsyncTask<String, Integer, String> {
+    private class NotifyBabyWarningResult extends AsyncTask<String, Integer, String> {
 
         LoadManager load;
 
@@ -93,27 +162,90 @@ public class DataResultActivity extends Activity {
 
 
         protected void onPostExecute(String result) {
-
             super.onPostExecute(result);
             Log.e("MenuLoadManger", "onPostexecute");
+
             try {
                 JSONObject jObj = new JSONObject(result);
                 String back = jObj.getString("back");
                 String front = jObj.getString("front");
                 String side = jObj.getString("side");
-//            JSONObject o = new JSONObject(result);
-                Log.e("PostExecute", back);
-                Log.e("PostExecute", front);
 
-                textView.setText("back is"+ back + "front is " + front + "side is"+ side);
+                double backValue = Double.parseDouble(back); // string을 long으로 형변환
+                double frontValue = Double.parseDouble(front);
+                double sideValue = Double.parseDouble(side);
+
+                if (backValue > frontValue && backValue > sideValue) { // 텐서플로 결과값중 back이 가장 높을 때
+                    if (!play) {
+                        mView.setVisibility(View.VISIBLE);
+                        showNotification();
+                        checkSoundMode();
+                        streamId = soundPool.play(soundId, 1.0F, 1.0F, 1, -1, 1.0F);
+                        play = true;
+                    }
+                    else {
+                        Log.e("background", "press confirm");
+                    }
+                }
             } catch (JSONException e) {
                 Log.e("MenuLoadManger", "postexecute error");
                 e.printStackTrace();
-
             }
-
         }
-
     }
 
+    public void checkSoundMode() { // 현재 기기 모드
+        switch (audioManager.getRingerMode()) {
+            case AudioManager.RINGER_MODE_NORMAL: // 소리모드
+                break;
+            case AudioManager.RINGER_MODE_SILENT: // 무음모드
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL); // 소리모드로 변경
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE: // 진동모드
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void showNotification() { // 팝업 알림
+        android.support.v4.app.NotificationCompat.Builder mBuilder =
+                new android.support.v4.app.NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.baby)
+                        .setContentTitle("Baby warning")
+                        .setContentText("Check your baby's posture!")
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setPriority(Notification.PRIORITY_HIGH);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, mBuilder.build());
+    }
+
+//    public void showDialog() { // 다이알로그 창 띄우기
+//        AlertDialog.Builder builder = new AlertDialog.Builder(mView.getContext());
+//        builder.setTitle("Baby warning");
+//        builder.setMessage("Check your baby's posture!");
+//        builder.setPositiveButton("Confirm",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        soundPool.stop(streamId); // 확인 누르면 알림음 종료
+//                    }
+//                });
+//        builder.setNegativeButton("아니오",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(getApplicationContext(), "아니오를 선택했습니다.", Toast.LENGTH_LONG).show();
+//                    }
+//                });
+//        builder.show();
+//    }
+
+    @Override
+    public void onDestroy() { // 서비스가 종료될때
+        super.onDestroy();
+
+//        soundPool.stop(streamId);
+//        mView.setVisibility(View.INVISIBLE);
+    }
 }
--- ImagineCup/app/src/main/java/com/example/myapplication/etc/LoadManager.java
+++ ImagineCup/app/src/main/java/com/example/myapplication/etc/LoadManager.java
@@ -16,19 +16,19 @@ import java.net.URL;
 
 public class LoadManager {
 
-    URL url;				//접속대상 서버주소를 가진 객체
+    URL url;                //접속대상 서버주소를 가진 객체
 
     static String testurl = "http://52.170.90.244/data";
-    HttpURLConnection conn;	//통신을 담당하는 객체
+    HttpURLConnection conn;    //통신을 담당하는 객체
 
-    BufferedReader buffer=null;
+    BufferedReader buffer = null;
     //필요한 객체 초기화
 
     public LoadManager() {
         try {
             Log.e("LoadManger", "url연결");
             url = new URL(testurl);
-            conn = (HttpURLConnection)url.openConnection();
+            conn = (HttpURLConnection) url.openConnection();
         } catch (MalformedURLException e) {
             Log.e("LoadManger", "error");
             e.printStackTrace();
@@ -38,14 +38,14 @@ public class LoadManager {
         }
     }
 
-    public String request(){
+    public String request() {
 
-        String data="";
+        String data = "";
         Log.e("LoadMangerRequest", "hey");
         try {
 
-            conn.connect();			//웹서버에 요청하는 시점
-            InputStream is = conn.getInputStream();	//웹서버로부터 전송받을 데이터에 대한 스트림 얻기
+            conn.connect();            //웹서버에 요청하는 시점
+            InputStream is = conn.getInputStream();    //웹서버로부터 전송받을 데이터에 대한 스트림 얻기
 
             //1byte기반의 바이트스트림이므로 한글이 깨진다.
             //따라서 버퍼처리된 문자기반의 스트림으로 업그레이드 해야 된다.
@@ -53,16 +53,16 @@ public class LoadManager {
             buffer = new BufferedReader(new InputStreamReader(is));
             //스트림을 얻어왔으므로, 문자열로 반환
             StringBuffer str = new StringBuffer();
-            String d=null;
-            while( (d=buffer.readLine()) != null){
+            String d = null;
+            while ((d = buffer.readLine()) != null) {
                 str.append(d);
             }
             data = str.toString();
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally{
-            if(buffer!=null){
+        } finally {
+            if (buffer != null) {
                 try {
                     buffer.close();
                 } catch (IOException e) {
